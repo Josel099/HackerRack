5049d51c9e864f753321e8cb4c0315cd1c93854d
Josel099 <joseltech2002@gmail.com>
2023-02-17 20:12:11 +0530

    add new problem

---
+++ src/Algorithems/SumOfarry.java
@@ -0,0 +1,27 @@
+package Algorithems;
+
+import java.util.Scanner;
+
+public class SumOfarry {
+
+	
+	public static void main(String[] args) {
+		
+		
+		double[] arr = new double[1000];
+		
+		Scanner sc=new Scanner(System.in);
+		double sum=0;
+		System.out.println("enter the size of array");
+		 int n=sc.nextInt();
+		 System.out.println("enter the elements of of array");
+		 for(int i=0;i<n;i++) {
+			 arr[i]= sc.nextDouble();
+			  sum=sum+arr[i];
+		 }
+		 System.out.println("The sum of the array is :" + sum);
+		 
+	
+	
+}
+}
