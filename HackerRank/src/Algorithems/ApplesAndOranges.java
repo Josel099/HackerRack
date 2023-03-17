ce079f89c8a87f0ff710994eb7ab59011da4439a
Josel Xavier <joseltech2002@gmail.com>
2023-03-17 16:38:52 +0530

    numberlineJump solved

--- HackerRank/src/Algorithems/ApplesAndOranges.java
+++ HackerRank/src/Algorithems/ApplesAndOranges.java
@@ -1,15 +1,6 @@
 package Algorithems;
 
-
-
-
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 class Result {
 
--- HackerRank/src/Algorithems/NumberLineJumps.java
+++ HackerRank/src/Algorithems/NumberLineJumps.java
@@ -1,5 +1,45 @@
 package Algorithems;
 
+import java.util.Scanner;
+
+class Result1 {
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+    // Write your code here
+    	int kang1 = x1+v1;
+    	int kang2 = x2+v2;
+    	for (int j=0 ; j<=10000 ; j++) {
+    		
+    		if (kang1==kang2) {
+    			return "Yes";
+    		}
+    		
+    		kang1+=v1;
+    		kang2+=v2;
+    		
+    		
+    	}
+		return "No";
+		
+
+    }
+}
+
 public class NumberLineJumps {
+
+
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x1 = sc.nextInt();
+		int v1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int v2 = sc.nextInt();
+		 String result = Result1.kangaroo(x1,v1,x2,v2);
+		System.out.println(result);
+		
+	}
 
 }
