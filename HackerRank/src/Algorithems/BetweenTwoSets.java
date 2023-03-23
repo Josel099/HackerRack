127788e8aca5cab1735af5d2485ab991ce47d1ff
Josel Xavier <joseltech2002@gmail.com>
2023-03-23 22:49:31 +0530

    file edited

---
+++ HackerRank/src/Algorithems/BetweenTwoSets.java
@@ -0,0 +1,23 @@
+package Algorithems;
+
+import java.util.Scanner;
+
+public class BetweenTwoSets {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int [] a = new int [n];
+		int [] b = new int [m];
+		
+		for() {
+			
+		}
+		
+	}
+	
+}
