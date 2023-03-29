b351998cbd4857ec97083617308fac24b73ca3d7
Josel Xavier <joseltech2002@gmail.com>
2023-03-29 20:55:46 +0530

    divisble paire done

---
+++ HackerRank/src/Algorithems/DivisibleSumPairs.java
@@ -0,0 +1,51 @@
+package Algorithems;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+class Result {
+	
+	
+	public static int divisibleCount(int k ,List<Integer>ar) {
+		
+		int count = 0 ; 
+		
+		for(int i =0; i<ar.size()-1; i++) {
+			
+			for(int j= i+1; j< ar.size(); j++) {
+				
+				if ((ar.get(i)+ar.get(j)) % k == 0) {
+					
+					count++ ; 
+				}
+			  }
+			}
+		return count;	
+	}
+}
+
+public class DivisibleSumPairs {
+	
+
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		ArrayList<Integer> ar = new ArrayList<Integer>(n);
+		
+		for (int i=0; i<n ; i++) {
+			
+			int temp = sc.nextInt();
+			ar.add(temp);
+		}
+		
+		
+		int result = Result.divisibleCount(k,ar);
+		System.out.println(result);
+		
+	}
+	
+}
