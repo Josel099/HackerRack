0cf1f078473484104259f2124049e5c20a6fee88
Josel Xavier <joseltech2002@gmail.com>
2023-04-18 00:15:54 +0530

    hlf

---
+++ HackerRank/src/Algorithems/DrawingBook.java
@@ -0,0 +1,63 @@
+package Algorithems;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class pages {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER pf
+     */
+
+    public static int pageCount(int n, int p) {
+		
+    int frontCount =0;
+    int backCount = 0;
+    	//from the frond of the book 
+    	for (int i = 1; i<=p; i++) {
+    		frontCount++;
+    	}
+    	frontCount = frontCount/2;
+ 	
+    	if(n %2 != 0) {
+        	for (int i = n; i>=p ; i--) {
+        	backCount++;	
+        	}
+    	}else {
+    		
+    	}
+
+    	
+    	
+    	
+    	
+  return frontCount;
+    }
+
+}
+
+public class  DrawingBook  {
+	
+	public static void main(String[] args) {
+		
+	
+    Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+
+        int result = pages.pageCount(n, p);
+
+        System.out.println(result);
+    }}
+
--- HackerRank/src/Java/JavaStrings.java
+++ HackerRank/src/Java/JavaStrings.java
@@ -19,12 +19,12 @@ public class JavaStrings {
         if(result>0) {
         System.out.println("Yes");
         }else
-        	System.out.println("NO");
+        	System.out.println("No");
         
         
         A = A.substring(0,1).toUpperCase()+A.substring(1);
-        B = B.substring(0,1)+B.substring(1);
-        System.out.println(A);
+        B = B.substring(0,1).toUpperCase()+B.substring(1);
+        System.out.println(A+" "+B);
         
     }
 }
