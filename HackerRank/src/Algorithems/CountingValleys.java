f502d9d6d10ce8defaaa5d7006d50e9ec8365b85
Josel Xavier <joseltech2002@gmail.com>
2023-06-01 23:37:47 +0530

    counting volleys completed

--- HackerRank/src/Algorithems/CountingValleys.java
+++ HackerRank/src/Algorithems/CountingValleys.java
@@ -1,5 +1,52 @@
 package Algorithems;
 
+import java.util.Scanner;
+
+class ValleysNo{
+	
+	public static int valleys(int s, String p ) {
+		
+		int valleycount=0;
+		int count =0;
+		
+		for(int i =0 ; i< p.length(); i++) {
+			
+			if(p.charAt(i) == 'D') {
+				
+				count=count-1;
+			}
+			else if(p.charAt(i) == 'U') {
+				
+				count=count+1;
+				if(count==0) {
+					valleycount+=1;
+				}
+			}
+			
+			
+		}
+		
+		
+		return valleycount;
+		
+	}
+	
+	
+	
+}
 public class CountingValleys {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		String p = sc.next();
+		sc.close();
+		
+		int result = ValleysNo.valleys(s,p);
+		System.out.println(result);
+		
+		
+	}
 
 }
