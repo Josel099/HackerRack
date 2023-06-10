aef5a13bcd6c1d1cbb328ec5cee700cad04341d7
Josel Xavier <joseltech2002@gmail.com>
2023-06-11 01:17:27 +0530

    cmpleted

--- HackerRank/src/Algorithems/ElectronicsShop.java
+++ HackerRank/src/Algorithems/ElectronicsShop.java
@@ -2,39 +2,41 @@ package Algorithems;
 
 import java.util.Scanner;
 
-public class ElectronicsShop {
+  class expensive{
 	
-	public class expensive{
+	  int getMoneySpent(int b , int[] keyboard, int[] usb,int n , int m) {
 		
-		 static int getMoneySpent(int b , int[] keyboard, int[] usb,int n , int m) {
-			
-			 int demo;
-			 int result= -1 ;
+		 int demo;
+		 int result= -1 ;
+		 
+		 for(int i=0 ; i< n;i++) {
 			 
-			 for(int i=0 ; i< n;i++) {
+			 
+			 for(int j=0;j<m;j++) {
 				 
+				 demo=keyboard[i]+usb[j];
 				 
-				 for(int j=0;j<m;j++) {
-					 
-					 demo=keyboard[i]+usb[j];
-					 
-					 if( demo>result && demo<=b) {
-						 result=demo;
-					 }
-					 
+				 if( demo>result && demo<=b) {
+					 result=demo;
 				 }
 				 
 			 }
-				 
 			 
-			 
-			 
-			 
-			 
-			 return result;
 		 }
-		
-	}
+			 
+		 
+		 
+		 
+		 
+		 
+		 return result;
+	 }
+	
+}
+
+public class ElectronicsShop {
+	
+	
 
 	public static void main(String[] args) {
 		
@@ -53,8 +55,9 @@ public class ElectronicsShop {
 		for(int i=0 ; i<m ; i++) {
 			usb[i]=sc.nextInt();
 		}
+		expensive result=new expensive();
 		
-		System.out.println(expensive.getMoneySpent(b, keyboard, usb,n,m));
+		System.out.println(result.getMoneySpent(b, keyboard, usb,n,m));
 		
 		
 		
